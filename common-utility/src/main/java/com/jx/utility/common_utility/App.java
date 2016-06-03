fca3a3c221d6da56523bd9d9be7962cbc5e90393
jiangxu2 <jiangxu2@centaline.com.cn>
2016-06-03 19:43:52 +0800

    maven依赖

---
+++ common-utility/src/main/java/com/jx/utility/common_utility/App.java
@@ -0,0 +1,13 @@
+package com.jx.utility.common_utility;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

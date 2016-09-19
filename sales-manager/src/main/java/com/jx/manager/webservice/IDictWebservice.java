91c6fa0815f048857409e40823d41bc18facec8c
jiangxu2 <jiangxu2@centaline.com.cn>
2016-09-19 09:38:17 +0800

    cxf接口

--- sales-manager/src/main/java/com/jx/manager/webservice/IDictWebservice.java
+++ sales-manager/src/main/java/com/jx/manager/webservice/IDictWebservice.java
@@ -1,8 +1,11 @@
 package com.jx.manager.webservice;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService
 public interface IDictWebservice {
-	public String test(String str);
+	@WebMethod
+	public String test(@WebParam(name = "username") String username);
 }
